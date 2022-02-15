40bf499383dcd0d8e853d47612ceb53bc19e0e97
GitHub Action <action@github.com>
2022-02-15 03:05:04 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "folder-alias-jb"
--- src/main/kotlin/com/github/muromirikka/folderaliasjb/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/muromirikka/folderaliasjb/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.muromirikka.folderaliasjb.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.muromirikka.folderaliasjb.MyBundle
 
 class MyApplicationService {
 
